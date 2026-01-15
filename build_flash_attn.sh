b98f3c6df5b260f584fc65ade3badd96be5d856d
Xu Bai <bxb1@spartan-gpgpu143.hpc.unimelb.edu.au>
2026-01-15 17:18:07 +1100

    up

--- build_flash_attn.sh
+++ build_flash_attn.sh
@@ -2,4 +2,4 @@ export FLASH_ATTN_HEAD_SIZE=128
 export FLASHATTENTION_ONLY_BF16=1
 export FLASH_ATTN_DISABLE_FA3=TRUE
 ccache -z
-uv pip install --no-build-isolation -v -e . > uv_build.log 2>&1
+uv pip install -v -e . > uv_build.log 2>&1
